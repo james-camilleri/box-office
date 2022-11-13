32080ac0a6d428a49e5940576c41aee4babffe74
James Camilleri <james@james.mt>
2022-11-13 19:16:07 +0000

    Fix cli imports

--- cms/sanity.cli.ts
+++ cms/sanity.cli.ts
@@ -1,8 +1,8 @@
-import {defineCliConfig} from 'sanity/cli'
+import { createCliConfig } from 'sanity/cli'
 
-export default defineCliConfig({
+export default createCliConfig({
   api: {
     projectId: '8biawkr2',
-    dataset: 'production'
-  }
+    dataset: 'production',
+  },
 })
