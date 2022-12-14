f9b2e3710ce5c4397456069b670768a46320195a
James Camilleri <james@james.mt>
2022-12-14 21:21:05 +0000

    Hide unneeded sanity actions

--- sites/cms/sanity.config.ts
+++ sites/cms/sanity.config.ts
@@ -1,10 +1,20 @@
 import { visionTool } from '@sanity/vision'
-import { defineConfig } from 'sanity'
+import { DocumentActionComponent, defineConfig } from 'sanity'
 import { deskTool } from 'sanity/desk'
 
+import { CreateBooking } from './desk/actions/create-booking'
 import { PublishConfig } from './desk/actions/publish-config'
 import { schemaTypes } from './schemas'
 import { structure } from './structure'
+
+enum ACTIONS {
+  PUBLISH = 'publish',
+  UNPUBLISH = 'unpublish',
+  DISCARD_CHANGES = 'discardChanges',
+  DUPLICATE = 'duplicate',
+  DELETE = 'delete',
+  RESTORE = 'restore',
+}
 
 export default defineConfig({
   name: 'default',
@@ -25,10 +35,32 @@ export default defineConfig({
 
   document: {
     actions(actions, context) {
+      // Use custom publish action for the configuration page.
+      // Keep the discard changes action and get rid of everything else.
       if (context.schemaType === 'pageConfigure') {
-        actions = actions.map((previousAction) =>
-          previousAction.action === 'publish' ? PublishConfig : previousAction,
+        return actions
+          .map((previousAction) =>
+            previousAction.action === ACTIONS.PUBLISH
+              ? PublishConfig
+              : previousAction.action === ACTIONS.DISCARD_CHANGES
+              ? previousAction
+              : null,
+          )
+          .filter(Boolean) as DocumentActionComponent[] // TS's silly filter thing again.
+      }
+
+      if (['booking'].includes(context.schemaType)) {
+        return [CreateBooking]
+      }
+
+      if (['pageConfigure'].includes(context.schemaType)) {
+        return actions.filter(({ action }) =>
+          [ACTIONS.PUBLISH, ACTIONS.DISCARD_CHANGES].includes(action as ACTIONS),
         )
+      }
+
+      if (['ticket', 'row', 'seat', 'section'].includes(context.schemaType)) {
+        return []
       }
 
       return actions
