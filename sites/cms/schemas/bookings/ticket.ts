60c129d34fb9f7c057ff75e69047bbce562d8c0d
James Camilleri <james@james.mt>
2022-12-15 23:47:22 +0000

    Refine ticket schema

--- sites/cms/schemas/bookings/ticket.ts
+++ sites/cms/schemas/bookings/ticket.ts
@@ -9,21 +9,35 @@ export default defineType({
       name: 'booking',
       type: 'reference',
       to: [{ type: 'booking' }],
+      validation: (Rule) => Rule.required(),
     }),
     defineField({
       name: 'show',
       type: 'reference',
       to: [{ type: 'show' }],
+      validation: (Rule) => Rule.required(),
     }),
     defineField({
       name: 'seat',
       type: 'reference',
       to: [{ type: 'seat' }],
+      validation: (Rule) => Rule.required(),
     }),
     defineField({
-      name: 'qrData',
-      title: 'QR code data',
-      type: 'string',
+      name: 'valid',
+      type: 'boolean',
+      initialValue: false,
     }),
+    defineField({
+      name: 'scanned',
+      type: 'boolean',
+      initialValue: false,
+    }),
+
+    // defineField({
+    //   name: 'qrData',
+    //   title: 'QR code data',
+    //   type: 'string',
+    // }),
   ],
 })
