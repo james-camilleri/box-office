4eba858ebf3d2d22de564962e1f1dcc3daffe2e8
James Camilleri <james@james.mt>
2022-12-17 01:07:15 +0000

    Remove unused routes

--- sites/web/src/routes/+page.ts
+++ sites/web/src/routes/+page.ts
@@ -4,13 +4,10 @@ import type { PageLoad } from './$types'
 
 export const prerender = true
 
-console.log('+++ THIS PAGE +++')
 export const load: PageLoad<ConfigurationFull> = async ({ fetch }) => {
-  console.log('+++ CALLING FETCH CONFIGURATION +++')
   const configuration = (await fetch('/api/config').then((payload) =>
     payload.json(),
   )) as ConfigurationFull
-  console.log('+++ CALLED FETCH CONFIGURATION +++')
 
   return { configuration }
 }
