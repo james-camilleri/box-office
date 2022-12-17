68347b006d45db1aaf619a5fcc7ba88e1a6f0d79
James Camilleri <james@james.mt>
2022-12-17 00:59:52 +0000

    Remove Sanity CDN warning

--- sites/web/src/routes/api/sanity.ts
+++ sites/web/src/routes/api/sanity.ts
@@ -10,15 +10,14 @@ const CLIENT_CONFIG = {
   dataset,
   apiVersion,
   token: SANITY_API_KEY,
+  useCdn: true,
 }
 
 interface SanityCompoundClient extends SanityClient {
   noCdn: SanityClient
 }
 
-// @ts-expect-error Sanity has trouble with nodenext export resolution.
 const sanity = createClient(CLIENT_CONFIG) as SanityCompoundClient
-// @ts-expect-error Sanity has trouble with nodenext export resolution.
 sanity.noCdn = createClient({ ...CLIENT_CONFIG, useCdn: false }) as SanityClient
 
 export { sanity }
