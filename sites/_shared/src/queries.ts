069549e88162450cdc70aca1c2d176d092cb9af3
James Camilleri <james@james.mt>
2022-12-17 01:29:17 +0000

    Get unavailable seats from tickets, not bookings

--- sites/_shared/src/queries.ts
+++ sites/_shared/src/queries.ts
@@ -2,17 +2,18 @@
  * Booked seats for a particular show.
  * @show the `_id` of the show
  */
-export const BOOKED_SEATS = '*[_type == "booking" && show._ref == $show][0].seats[]._ref'
+export const BOOKED_SEATS = '*[_type == "ticket" && valid && show._ref == $show][].seat._ref'
 
 /**
  * Unbooked seats for a particular show.
  * @show the `_id` of the show
  */
 export const UNBOOKED_SEATS =
-  '*[_type == "seat" && !(_id in *[_type == "booking" && show._ref == $show][0].seats[]._ref)][]._id'
+  '*[_type == "seat" && !(_id in *[_type == "ticket" && valid && show._ref == $show][].seat._ref)][]._id'
 
 /**
  * Check if a customer for the given email already exists.
  * @email the email to test for
  */
-export const CUSTOMER_EXISTS = 'defined(*[_type == "customer" && email == $email][0]._id)'
+export const CUSTOMER_EXISTS =
+  'defined(*[_type == "customer" && !(_id in path("drafts.**")) && email == $email][0]._id)'
