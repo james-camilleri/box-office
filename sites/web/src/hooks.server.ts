b8fb9a95dd2997f1af2bb843c9e3fb13aa9b9233
James Camilleri <james@james.mt>
2023-01-22 22:38:36 +0000

    Remove logs

--- sites/web/src/hooks.server.ts
+++
@@ -1,8 +0,0 @@
-/** @type {import('@sveltejs/kit').HandleFetch} */
-export function handleFetch(arg) {
-  console.log('arg.request', arg.request)
-  console.log('arg.event', arg.event)
-  const { request, fetch } = arg
-
-  return fetch(request)
-}
