b1ef7cdc3990d925da21c8fd3a03167426fadf02
James Camilleri <james@james.mt>
2023-01-22 16:08:41 +0000

    Send invoice alongside tickets

--- sites/web/src/routes/api/tickets/email/+server.ts
+++ sites/web/src/routes/api/tickets/email/+server.ts
@@ -2,12 +2,14 @@ import { error, type RequestHandler } from '@sveltejs/kit'
 import CONFIG from '$lib/config.js'
 import juice from 'juice'
 import nodemailer from 'nodemailer'
+import { parseFullName } from 'parse-full-name'
 import { BOOKING_DETAILS } from 'shared/queries'
 import type { BookingDetails, ConfigurationFull, Ticket } from 'shared/types'
 
 import { getCrossOriginHeader } from '../../cors.js'
 import { sanity } from '../../sanity.js'
-import Email from './template/Email.svelte'
+import TicketEmail from './templates/Tickets.svelte'
+import InvoiceEmail from './templates/Invoice.svelte'
 
 interface BookingPayload {
   bookingId: string
@@ -23,31 +25,21 @@ export const POST: RequestHandler = async ({ request, fetch }) => {
       ((await sanity.fetch(BOOKING_DETAILS, { bookingId })) as BookingDetails[])[0],
     ])
 
-    const { showName, showLocation, mapUrl } = config
+    const { name, email, date, show, discount } = bookingDetails
+    const { showName, showLocation, mapUrl, priceTiers, priceConfiguration } = config
 
-    // TODO: Time Zones. Again.
-    const { html: rawHtml, css } = Email.render({
-      name: bookingDetails.name,
-      event: {
-        name: showName,
-        date: new Date(bookingDetails.date.split('T')[0]).toLocaleDateString(),
-        time: bookingDetails.date.split('T')[1],
-        location: showLocation,
-        map: mapUrl,
-      },
-      tickets,
-    })
-
-    const html = juice(`<style>${css.code}</style>${rawHtml}`)
+    const { first, middle, nick } = parseFullName(name)
+    const firstName =
+      middle && nick
+        ? `${first} ${middle} ${nick}`
+        : middle
+        ? `${first} ${middle}`
+        : nick
+        ? `${first} ${nick}`
+        : first ?? ''
 
     const { host, port } = CONFIG.EMAIL
     const { MAILJET_API_KEY, MAILJET_SECRET_KEY } = process.env
-
-    // if (!name || !email || !subject || !message) {
-    //   throw new Error('Missing required fields')
-    // }
-
-    const subject = `Your tickets for ${showName}`
 
     const transport = nodemailer.createTransport({
       name: 'tickets@arthaus.mt',
@@ -61,17 +53,66 @@ export const POST: RequestHandler = async ({ request, fetch }) => {
       secure: true,
     })
 
-    await transport.sendMail({
-      from: 'Arthaus <tickets@arthaus.mt>',
-      replyTo: 'tickets@arthaus.mt',
-      to: `${bookingDetails.name} <${bookingDetails.email}>`,
-      subject,
-      html,
-    })
+    await Promise.all([
+      transport.sendMail(
+        generateEmail(
+          name,
+          email,
+          `Your tickets for ${showName}`,
+          generateEmailHtml(
+            TicketEmail.render({
+              name: firstName,
+              event: {
+                name: showName,
+                // TODO: Time Zones. Again.
+                date: new Date(date).toLocaleDateString(),
+                time: new Date(date).toLocaleTimeString(),
+                location: showLocation,
+                map: mapUrl,
+              },
+              tickets,
+            }),
+          ),
+        ),
+      ),
+      transport.sendMail(
+        generateEmail(
+          name,
+          email,
+          `Your invoice for ${showName}`,
+          generateEmailHtml(
+            InvoiceEmail.render({
+              name: firstName,
+              event: { name: showName },
+              show,
+              tickets,
+              priceTiers,
+              priceConfiguration,
+              discount,
+            }),
+          ),
+        ),
+      ),
+    ])
   } catch (e) {
     throw error(500, e)
   }
+
   return new Response(null, {
     headers: getCrossOriginHeader(request.headers),
   })
+}
+
+function generateEmail(name: string, email: string, subject: string, html: string) {
+  return {
+    from: 'Arthaus <tickets@arthaus.mt>',
+    replyTo: 'tickets@arthaus.mt',
+    to: `${name} <${email}>`,
+    subject,
+    html,
+  }
+}
+
+function generateEmailHtml({ rawHtml, css }: { rawHtml: string; css: { code: string } }) {
+  return juice(`<style>${css.code}</style>${rawHtml}`)
 }
