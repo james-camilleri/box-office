f9aa63ad815e4ee7bafd8e99f2622551712dc3f5
James Camilleri <james@james.mt>
2023-01-29 15:45:54 +0000

    Remove test log

--- sites/web/src/routes/api/config/+server.ts
+++ sites/web/src/routes/api/config/+server.ts
@@ -17,7 +17,6 @@ const CONFIG_QUERY = `*[_id == '${PAGE_ID}']{
 
 /** @type {import('./$types').RequestHandler} */
 export async function GET() {
-  console.log('process.env', process.env)
   const configuration = await sanity.fetch(CONFIG_QUERY)
 
   return json({
