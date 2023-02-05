2abd001897842ed36df6949253211a0102b76ba4
James Camilleri <james@james.mt>
2023-02-05 01:20:57 +0000

    Add array key sanity util

--- sites/_shared/src/utils/sanity.ts
+++ sites/_shared/src/utils/sanity.ts
@@ -1,6 +1,11 @@
+import { customAlphabet } from 'nanoid'
+
 export function createReference(id: string) {
   return {
     _type: 'reference' as const,
     _ref: id,
   }
 }
+
+const SANITY_KEY_ALPHABET = '0123456789abcdefghijklmnopqrstuvwxyz'
+export const generateArrayKey = customAlphabet(SANITY_KEY_ALPHABET, 32)
