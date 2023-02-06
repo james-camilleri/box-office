2666959ccb05733f47e1ee0fd7cb85f56fcea0e6
James Camilleri <james@james.mt>
2023-02-06 22:33:40 +0000

    Add price tiers to seating plan

---
+++ sites/web/src/lib/components/seatplan/PriceTiers.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+  import type { PriceTier } from 'shared/types'
+
+  export let priceTiers: PriceTier[]
+</script>
+
+<div class="price-tiers">
+  {#each priceTiers as { colour, name, price }}
+    <div class="price-tier">
+      <div class="key" style:--colour={colour} />
+      <div class="price">€{price}</div>
+    </div>
+  {/each}
+</div>
+
+<style>
+  .price-tiers {
+    display: flex;
+    gap: var(--xl);
+    padding: var(--md);
+    margin: 0 auto;
+    background: var(--light-3);
+    border-radius: var(--border-radius);
+  }
+
+  .price-tier {
+    display: flex;
+    gap: var(--xxs);
+    align-items: center;
+  }
+
+  .key {
+    width: 1.2rem;
+    height: 1.2rem;
+    background: var(--colour);
+    border-radius: 50%;
+  }
+
+  .price {
+    font-weight: bold;
+  }
+</style>
