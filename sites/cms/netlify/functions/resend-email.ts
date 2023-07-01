40c716c528884599331bf49e2a53601b738d908a
James Camilleri <james@james.mt>
2023-07-01 22:47:42 +0200

    Fix illegal invocation error

--- sites/cms/netlify/functions/resend-email.ts
+++ sites/cms/netlify/functions/resend-email.ts
@@ -3,13 +3,13 @@ import { Handler } from '@netlify/functions'
 const APP_URL = 'https://tickets.arthaus.mt'
 
 export const handler: Handler = async (event) => {
-  const { status, json } = await fetch(`${APP_URL}/api/booking/email`, {
+  const response = await fetch(`${APP_URL}/api/booking/email`, {
     method: 'POST',
     body: event.body,
   })
 
   return {
-    statusCode: status,
-    body: await json(),
+    statusCode: response.status,
+    body: await response.json(),
   }
 }
