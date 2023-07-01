82245ff84cee5f5d08e8f97947ad72ea52042b94
James Camilleri <james@james.mt>
2023-07-01 22:58:13 +0200

    Spoof request origin

--- sites/cms/netlify/functions/resend-email.ts
+++ sites/cms/netlify/functions/resend-email.ts
@@ -5,6 +5,9 @@ const APP_URL = 'https://tickets.arthaus.mt'
 export const handler: Handler = async (event) => {
   const response = await fetch(`${APP_URL}/api/booking/email`, {
     method: 'POST',
+    headers: {
+      origin: APP_URL,
+    },
     body: event.body,
   })
 
