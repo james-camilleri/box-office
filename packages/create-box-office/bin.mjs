bb6676140dd8ddaa0e180efba98ff99f8fd39633
James Camilleri <james@james.mt>
2023-08-18 08:12:12 +0100

    Update installation script

--- packages/create-box-office/bin.mjs
+++ packages/create-box-office/bin.mjs
@@ -22,6 +22,9 @@ async function initialise() {
   await createProjectDir(cwd)
 
   const projectInfo = await getProjectInfo(defaults)
+  const environmentVariables = Object.entries(projectInfo)
+    .filter(([key]) => key[0] === key[0].toUpperCase())
+    .reduce((envVariables, [key, value]) => ({ ...envVariables, [key]: value }), {})
 
   const packageName = projectInfo.name
     .trim()
@@ -37,12 +40,22 @@ async function initialise() {
   // npm won't publish .gitignore, so we need to save
   // it under a different name and then rename it.
   await fs.rename(`${cwd}/gitignore`, `${cwd}/.gitignore`)
+  await fs.rename(`${cwd}/sites/cms/gitignore`, `${cwd}/sites/cms/.gitignore`)
+  // await fs.rename(`${cwd}/sites/web/gitignore`, `${cwd}/sites/web/.gitignore`)
 
   console.log()
   console.log('Installing dependencies.')
   await installDependencies(
     {
-      dependencies: ['@james-camilleri/replace-sanity-favicon', 'nodemailer'],
+      dependencies: [
+        '@james-camilleri/replace-sanity-favicon',
+        'nodemailer',
+        'react-dom',
+        'react-is',
+        'react',
+        'sanity',
+        'styled-components@^5.2',
+      ],
       devDependencies: ['@netlify/functions', '@the-gods/box-office', 'env-cmd', 'ts-node'],
     },
     `${cwd}/sites/cms`,
@@ -58,24 +71,22 @@ async function initialise() {
   console.log('Initialising Sanity project.')
   const sanityConfig = await configureSanity({ name: projectInfo.name, dest: `${cwd}/sites/cms` })
 
-  // Re-copy files that `create sanity` has overwritten.
-  await copyDir(
-    fileURLToPath(new URL(`./template/sites/cms`, import.meta.url).href),
-    `${cwd}/sites/cms`,
-  )
-
   const dictionary = {
     ...sanityConfig,
     name: packageName,
   }
+  console.log('dictionary', dictionary)
 
   console.log()
   console.log('Replacing template placeholders.')
   await Promise.all(
-    ['/sites/cms/package.json', '/sites/web/package.json'].map((path) =>
+    ['sites/cms/package.json', 'sites/cms/sanity.config.ts', 'sites/web/package.json'].map((path) =>
       replacePlaceholdersInFile(`${cwd}/${path}`, dictionary),
     ),
   )
+
+  console.log()
+  console.log('Writing environment variables to .env file.')
 
   if (projectInfo.initGit) {
     console.log()
@@ -87,6 +98,8 @@ async function initialise() {
     console.log()
     console.log('Configuring Netlify.')
     await configureNetlify(`${cwd}/sites/web`, `${cwd}/sites/cms`)
+
+    // TODO: Post ENV variables to Netlify
   }
 }
 
