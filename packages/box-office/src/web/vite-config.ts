fb422257efbae32c05da2ff8c2c3cde5b1cbb3ac
James Camilleri <james@james.mt>
2023-08-20 01:06:17 +0100

    Update create config types

--- packages/box-office/src/web/vite-config.ts
+++ packages/box-office/src/web/vite-config.ts
@@ -2,7 +2,7 @@ import type { UserConfig } from 'vite'
 
 import { sveltekit } from '@sveltejs/kit/vite'
 
-export function createViteConfig(config: UserConfig) {
+export function createViteConfig(config?: UserConfig) {
   return {
     ...config,
     plugins: [sveltekit(), ...(config?.plugins || [])],
