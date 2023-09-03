fb5b5d78ca0ca2ba2884d54039d743005e44fb78
James Camilleri <james@james.mt>
2023-09-03 11:21:22 +0100

    Update components export again

---
+++ packages/box-office/src/web/src/components.ts
@@ -0,0 +1,3 @@
+export { default as Section } from './lib/components/seatplan/Section.svelte'
+export { default as Row } from './lib/components/seatplan/Row.svelte'
+export { default as Seat } from './lib/components/seatplan/Seat.svelte'
