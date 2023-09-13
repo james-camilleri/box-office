9164e282356da7f6c3453503e149db1e0a15e4f0
James Camilleri <james@james.mt>
2023-09-13 23:58:06 +0100

    Add campaigns to payment intent

--- packages/box-office/src/web/src/routes/(public)/+server.ts
+++ packages/box-office/src/web/src/routes/(public)/+server.ts
@@ -1,18 +1,21 @@
-import { json } from '@sveltejs/kit'
-import { STRIPE_LIVE_SECRET_KEY, STRIPE_TEST_SECRET_KEY } from '$env/static/private'
-import { PUBLIC_USE_STRIPE_TEST, PUBLIC_STRIPE_CONNECT_ID } from '$env/static/public'
 import type { Colour, ConfigurationFull, Discount, Seat, WebConfigurationRaw } from '$shared/types'
-import { getSeatPrice, getTotals } from '$shared/utils'
+import type { RequestHandler } from './$types.js'
+
+import { json } from '@sveltejs/kit'
 import Stripe from 'stripe'
 
-import { sanity } from '../api/sanity.js'
-import type { RequestHandler } from './$types.js'
+import { STRIPE_LIVE_SECRET_KEY, STRIPE_TEST_SECRET_KEY } from '$env/static/private'
+import { PUBLIC_USE_STRIPE_TEST, PUBLIC_STRIPE_CONNECT_ID } from '$env/static/public'
 import { WEBSITE_CONFIGURATION } from '$shared/queries'
+import { getSeatPrice, getTotals } from '$shared/utils'
+
+import { sanity } from '../api/sanity.js'
 
 interface Payload {
   seats: Seat[]
   show: string
   discountCode?: string
+  campaigns?: string[]
 }
 
 const API_KEY =
@@ -23,7 +26,7 @@ const stripe = new Stripe(API_KEY)
 
 export const POST: RequestHandler = async (event) => {
   const { request, fetch } = event
-  const { seats, show, discountCode } = (await request.json()) as Payload
+  const { seats, show, discountCode, campaigns } = (await request.json()) as Payload
 
   const requests = [(await fetch('/api/config')).json()]
   discountCode &&
@@ -47,6 +50,7 @@ export const POST: RequestHandler = async (event) => {
         show,
         seatIds: JSON.stringify(seats.map((seat) => seat._id)),
         discount: JSON.stringify(discount),
+        campaigns: JSON.stringify(campaigns),
       },
       application_fee_amount: Math.floor((applicationFee ?? 0) * 100),
     },
